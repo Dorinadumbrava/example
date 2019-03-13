930efcc644541fe463dde1c323096d3983c0373b
Dorina <drndumbrava@gmail.com>
2019-03-13 17:26:28 +0200

    tested validation

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,4 +1,5 @@
 import { ApplicationRef, Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Model } from './repository.model';
 import { Product } from './product.model';
 
@@ -8,21 +9,41 @@ import { Product } from './product.model';
 })
 
 export class ProductComponent {
- 
+
  model: Model = new Model();
 
     targetName = 'Kayak';
     selectedProduct: string;
     newProduct: Product = new Product();
+    formSubmitted = false;
+
+    submitForm(form: NgForm) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.reset();
+            this.formSubmitted = false;
+        }
+    }
+
+    getFormValidationMessages(form: NgForm): string[] {
+        const messages: string[] = [];
+        Object.keys(form.controls).forEach(k => {
+            this.getValidationMessages(form.controls[k], k)
+ .forEach(m => messages.push(m));
+ });
+        return messages;
+ }
 
     get jsonProduct() {
         return JSON.stringify(this.newProduct);
     }
 
-    addProduct(p: Product){
-        console.log("New Product: " + this.jsonProduct);
+    addProduct(p: Product) {
+        console.log('New Product: ' + this.jsonProduct);
     }
-    
+
     getProduct(key: number): Product {
         return this.model.getProduct(key);
     }
@@ -34,4 +55,29 @@ export class ProductComponent {
     getSelected(product: Product): boolean {
         return product.name === this.selectedProduct;
     }
-}
+
+    getValidationMessages(state: any, thingName?: string) {
+        const thing: string = state.path || thingName;
+        const messsages: string[] = [];
+        if (state.errors) {
+// tslint:disable-next-line: forin
+               for (const errorName in state.errors) {
+                   switch (errorName) {
+                       case 'required':
+                       messsages.push(`You must enter a ${thing}`);
+                       break;
+                       case 'minlength':
+                       messsages.push(`A ${thing} must be at least
+                       ${state.errors.minlength.requiredLength} characters`);
+                       break;
+                       case 'pattern':
+                       messsages.push(`The ${thing} contains
+                       illegal characters`);
+                       break;
+                   }
+               }
+           }
+        return messsages;
+        }
+    }
+
