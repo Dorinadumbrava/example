1e264e9bd1adc565ca767f4a7787983161e4d1a0
Dorina <drndumbrava@gmail.com>
2019-03-13 11:42:06 +0200

    modified component, template2

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,21 +1,44 @@
 import { ApplicationRef, Component } from '@angular/core';
 import { Model } from './repository.model';
 import { Product } from './product.model';
+
 @Component({
  selector: "app",
  templateUrl: 'template.html'
 })
+
 export class ProductComponent {
- model: Model = new Model();
  constructor(ref: ApplicationRef) {
  ( window as any).appRef = ref;
  ( window as any).model = this.model;
  }
- getProductByPosition(position: number): Product {
+ model: Model = new Model();
+
+    targetName = 'Kayak';
+    counter: number = 1;
+
+    getProductByPosition(position: number): Product {
     return this.model.getProducts()[position];
     }
-    getClassesByPosition(position: number): string {
-    const product = this.getProductByPosition(position);
-    return 'p-2 ' + (product.price < 50 ? 'bg-info' : 'bg-warning');
+
+    getProduct(key: number): Product {
+        return this.model.getProduct(key);
     }
-   }
+
+    getProducts(): Product [] {
+        return this.model.getProducts();
+    }
+
+    getProductCount(): number {
+        console.log('getProductCount invoked');
+        return this.getProducts().length;
+    }
+
+    getKey(index: number, product: Product) {
+        return product.id;
+    }
+
+    get nextProduct(): Product {
+        return this.model.getProducts().shift();
+    }
+}
